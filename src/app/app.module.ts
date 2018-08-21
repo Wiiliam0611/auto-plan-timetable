ddc9102f027dd6e81b30f2400c3c45c6c4f5d07a
Wiiliam0611 <william.yang2008@yahoo.com>
2018-08-21 20:17:19 +1000

    Initiate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { NavComponent } from './nav/nav.component';
+import { SubjectsListComponent } from './subjects-list/subjects-list.component';
+import { TimetableComponent } from './timetable/timetable.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { NavigatorComponent } from './navigator/navigator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ContentComponent,
+    NavComponent,
+    SubjectsListComponent,
+    TimetableComponent,
+    SearchBarComponent,
+    NavigatorComponent
   ],
   imports: [
     BrowserModule
